ff8ce02aa06ca4c2a05d3a64dc15f1cc516b8fda
arel10 <afchaarelpratama@gmail.com>
2025-07-28 21:46:28 +0700

    ubah api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Use full URL for development
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
